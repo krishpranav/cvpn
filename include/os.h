d0cca4b4240dd664cedba25864ae282839fbf4a0
krishpranav <krisna.pranav@gmail.com>
2021-08-27 18:15:41 +0530

    include: os header

--- include/os.h
+++ include/os.h
@@ -18,15 +18,20 @@ typedef struct Cmds {
 
 Cmds firewall_rules_cmds(int is_server);
 
+int shell_cmd(const char *substs[][2], const char *args_str, int silent);
+
 const char *get_default_gw_ip(void);
 
 const char *get_default_ext_if_name(void);
 
-
 int tcp_opts(int fd);
+
+int tun_create(char if_name[IFNAMSIZ], const char *wanted_name);
+
+int tun_set_mtu(const char *if_name, int mtu);
 
 ssize_t tun_read(int fd, void *data, size_t size);
 
-size_t tun_write(int fd, const void *data, size_t size);
+ssize_t tun_write(int fd, const void *data, size_t size);
 
-#endif 
+#endif
