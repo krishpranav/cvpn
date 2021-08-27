0e1d0619fc9c02b5d6ffd001ebf34efb00c169a7
krishpranav <krisna.pranav@gmail.com>
2021-08-27 18:13:47 +0530

    include: vpn header

--- include/vpn.h
+++ include/vpn.h
@@ -25,7 +25,35 @@
 #include <time.h>
 #include <unistd.h>
 
-#define VERSION_STRING "1.0.0"
+#ifdef __linux__
+#include <linux/if_tun.h>
+#endif
+
+#ifdef __APPLE__
+#include <net/if_utun.h>
+#include <sys/kern_control.h>
+#include <sys/sys_domain.h>
+#endif
+
+#define VERSION_STRING "0.1.4"
+
+#ifdef __NetBSD__
+#define DEFAULT_MTU 1500
+#else
+#define DEFAULT_MTU 9000
+#endif
+#define RECONNECT_ATTEMPTS 100
+#define TAG_LEN 6
+#define MAX_PACKET_LEN 65536
+#define TS_TOLERANCE 7200
+#define TIMEOUT (60 * 1000)
+#define ACCEPT_TIMEOUT (10 * 1000)
+#define OUTER_CONGESTION_CONTROL_ALG "bbr"
+#define BUFFERBLOAT_CONTROL 1
+#define NOTSENT_LOWAT (128 * 1024)
+#define DEFAULT_CLIENT_IP "192.168.192.1"
+#define DEFAULT_SERVER_IP "192.168.192.254"
+#define DEFAULT_PORT "443"
 
 #if defined(__BYTE_ORDER__) && defined(__ORDER_BIG_ENDIAN__) && \
     __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__ && !defined(NATIVE_BIG_ENDIAN)
